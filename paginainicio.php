8c39d9eb23ee15571ceca9d570e06d8986781bf6
LuciaAnsino <e.luciaansino@go.ugr.es>
2023-05-25 15:23:26 +0200

    HTML he hecho la parte fija, quedaria hacer lo de dentro que depende de la página en la que estes

---
+++ paginainicio.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Página Principal</title>
+</head>
+<body>
+	<header>
+		<img src="icono_incidencia.jpg" alt="Altavoz de queja">
+		<h1>Por un municipio mejor</h1>
+		<img src="icono.png" alt="Imagen incidencia">
+		<h2> Informa de cualquier incidencia sobre el servicio público para ayudar a mejorarlo</h2>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="">Ver incidencias</a></li>
+			<li><a href="">Nueva incidencia</a></li>
+			<li><a href="">Más incidencias</a></li>
+			<li><a href="">Gestón de usuarios</a></li>
+			<li><a href="">Ver log</a></li>
+			<li><a href="">Gestion de BBDD</a></li>
+		</ul>
+	</nav>
+	<div>
+		<section>
+		</section>
+		<aside>
+			<div>
+				<form action="" method="POST">
+					<label>Email: <input type="email" name="email" placeholder="Introduzca su email"></label>
+					<label>Clave: <input type="password" name="clave" placeholder="Introduzca su clave" ></label>
+					<input type="submit" value="Login"/>
+				</form>
+			</div>
+			<div>
+				<ol>Ranking de incidencias
+					<li>User1</li>
+					<li>User2</li>
+					<li>Ueser3</li>
+				</ol>
+				<ol>Ranking de opiniones
+					<li>User1</li>
+					<li>User2</li>
+					<li>Ueser3</li>
+				</ol>
+				<p>Incidencias pendientes: x</p>
+				<p>Incidencias resueltas: x</p>
+			</div>
+		</aside>
+	</div>
+	<footer>
+		<div>
+			<ul>
+				<li>© 2023 Lucía Ansino Ariza y Mario Guisado</li>
+				<li><a href="">Protección de datos</a></li>
+				<li><a href="">Fuentes Web</a></li>
+				<li><a href="">Aviso legal</a></li>
+				<li><a href="">Políticas de cookies</a></li>
+			</ul>
+		</div>
+		<div>
+			<a href=""><img src="./Twitter.png"></a>
+			<a href=""><img src="./facebook.png"></a>
+			<a href=""><img src="./YouTube.png"></a>
+		</div>
+	</footer>
+</body>
+</html>
